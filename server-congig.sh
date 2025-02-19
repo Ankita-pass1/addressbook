d2b04495b773fa61a8263753f06ad85754c12ca5
unknown <dassankita9163@gmail.com>
2025-02-20 03:13:48 +0530

    Modified Server.congig file with docker installation and build

--- server-congig.sh
+++ server-congig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Define project directory and Git repository URL
-PROJECT_DIR="addressbook"
-GIT_REPO_URL="https://github.com/Ankita-pass1/addressbook.git"
-BRANCH_NAME="b1"
+#PROJECT_DIR="addressbook"
+#GIT_REPO_URL="https://github.com/Ankita-pass1/addressbook.git"
+#BRANCH_NAME="b1"
 
 # Update package list and upgrade existing packages
 echo "Updating package list..."
@@ -20,6 +20,8 @@ sudo yum update -y
 # Install Git
 echo "Installing Git..."
 sudo yum install -y git
+sudo yum install docker -y
+
 
 # Verify installations
 echo "Verifying installations..."
@@ -27,16 +29,21 @@ echo "Verifying installations..."
 #mvn -version
 git --version
 
+#start docker
+echo "Starting Docker"
+sudo systemctl start docker
+
 # Check if the project directory exists
-if [ -d "$PROJECT_DIR" ]; then
+if [ -d addressbook ]; then
     echo "Project directory exists. Pulling the latest changes from the repository..."
-    cd "$PROJECT_DIR"
-    git checkout "$BRANCH_NAME"  # Ensure we are on the correct branch
-    git pull origin "$BRANCH_NAME"
+    cd /home/ec2-user/addressbook
+    #git checkout "$BRANCH_NAME"  # Ensure we are on the correct branch
+    git pull origin b1
 else
     echo "Project directory does not exist. Cloning the repository..."
-    git clone -b "$BRANCH_NAME" "$GIT_REPO_URL" "$PROJECT_DIR"
-    cd "$PROJECT_DIR"
+    git clone https://github.com/Ankita-pass1/addressbook.git 
+    cd /home/ec2-user/addressbook
+    git checkout b1
 fi
 
 # Run Maven package
